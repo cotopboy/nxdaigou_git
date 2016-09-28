8adf13f0cebf91912078bba4d58bb508cc96c853
Ruifeng Zhang <ruifeng.zhang.cz@googlemail.com>
2016-09-28 15:17:47 +0200

    save

---
+++ daigou.main/XamlResources/TestingUserControl.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using daigou.modules.Product;
+using daigou.modules.Bill;
+
+namespace daigou.main.XamlResources
+{
+    /// <summary>
+    /// Interaction logic for TestingUserControl.xaml
+    /// </summary>
+    public partial class TestingUserControl : UserControl
+    {
+        public TestingUserControl()
+        {
+            InitializeComponent();
+            
+            //this.DataContext = vm;
+        }
+    }
+}
