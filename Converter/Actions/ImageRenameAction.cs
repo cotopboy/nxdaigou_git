ea22c5ac72b97d8ad4f3d6778a749effd68734b9
Ruifeng Zhang <ruifeng.zhang.cz@googlemail.com>
2016-11-14 16:53:59 +0100

    s
    s

---
+++ Converter/Actions/ImageRenameAction.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using daigou.dal.DaigouDataFile;
+using Utilities.IO;
+using System.IO;
+
+namespace Converter.Actions
+{
+    public class ImageRenameAction
+    {
+
+        private List<string> delList = new List<string>();
+
+        public void Start()
+        {
+            FileDBMgr fileDBMgr = new FileDBMgr();
+            var db = fileDBMgr.LoadFileDB();
+
+            int index = 1;
+
+            foreach (var product in db.ProductList)
+            {
+                if (product.Photo.Length == 0) continue;
+
+                string oldName = product.Photo.Trim();
+
+                string newName = index.ToString()+ Path.GetExtension(oldName);
+
+                if (ReNameFile(oldName, "x" + newName))
+                {
+                    product.Photo = "images/" + "x" + newName;
+                }
+                else
+                {
+                    
+                }
+
+                index++;
+            }
+
+            fileDBMgr.Save();
+
+            delList.ForEach(x => File.Delete(x));
+        }
+
+        private bool ReNameFile(string oldName, string newName)
+        {
+            var fullPath = DirectoryHelper.CombineWithCurrentExeDir(oldName);
+
+            
+            var oldFilename = Path.GetFileName(fullPath);
+
+            string newPath = fullPath.Replace(oldFilename, newName);
+
+            try
+            {
+                File.Copy(fullPath, newPath);
+
+                this.delList.Add(fullPath);
+                return true;
+            }
+            catch(Exception exp)
+            {
+                Console.WriteLine(fullPath);
+                return false;
+            }
+        }
+    }
+}
--- Converter/Program.cs
+++ Converter/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using FileHelpers;
 using daigou.dal.DaigouDataFile;
 using daigou.domain;
+using Converter.Actions;
 
 namespace Converter
 {
@@ -12,105 +13,9 @@ namespace Converter
     {
         static void Main(string[] args)
         {
-            FileHelperEngine<CsvProduct> engine = new FileHelperEngine<CsvProduct>();
-            engine.Encoding =  Encoding.UTF8;
-            engine.ErrorManager.ErrorMode = ErrorMode.ThrowException;
+            
 
-            CsvProduct[] res = engine.ReadFile("csvProduct.txt");
-
-            if (engine.ErrorManager.ErrorCount > 0)
-                engine.ErrorManager.SaveErrors("Errors.txt");
-
-            Console.WriteLine(res.Count());
-
-            FileDBMgr file = new FileDBMgr();
-            file.LoadFileDB();
-
-            FileProductRepository prodoctRepo = new FileProductRepository(file);
-
-            foreach (var item in res)
-            {
-                Product product = new Product() 
-                {
-                     ApplicableCrowd = item.howbig,
-                      Brand = item.brand,
-                       Code = item.code,
-                         GrossWeight = (int) (item.weight ?? -1m),
-                          ImportPrice = item.inPrice ?? 0,
-                           Name = item.name,
-                            PackingCost = item.pcost ?? 0,
-                             PriceAdaption = item.adaption ??0,
-                              ServiceRate = item.drate ??1,
-                               Spec = item.spicification                               
-                           
-                           
-                };
-
-                prodoctRepo.Add(product);
-                
-            }
-
-
-            file.Save();
-
-
-
-
+            Console.ReadLine();
         }
-    }
-
-    [DelimitedRecord(";")]
-    public sealed class CsvProduct
-    {
-
-        [FieldTrim(TrimMode.Both)]
-        [FieldOptional]
-        public String code;
-
-        [FieldTrim(TrimMode.Both)]
-        [FieldOptional]
-        public String brand;
-
-        [FieldTrim(TrimMode.Both)]
-        [FieldOptional]
-        public String howbig;
-
-        [FieldTrim(TrimMode.Both)]
-        [FieldOptional]
-        public String name;
-
-        [FieldTrim(TrimMode.Both)]
-        [FieldOptional]
-        public String spicification;
-
-        [FieldTrim(TrimMode.Both)]
-        [FieldOptional]
-        public decimal ? sellPrice;
-
-        [FieldTrim(TrimMode.Both)]
-        [FieldOptional]
-        public decimal? inPrice;
-
-        [FieldTrim(TrimMode.Both)]
-        [FieldOptional]
-        public decimal? drate;
-
-        [FieldTrim(TrimMode.Both)]
-        [FieldOptional]
-        public decimal? pcost;
-
-        [FieldTrim(TrimMode.Both)]
-        [FieldOptional]
-        public decimal? euroPrice;
-
-        [FieldTrim(TrimMode.Both)]
-        [FieldOptional]
-        public decimal? adaption;
-
-        [FieldTrim(TrimMode.Both)]
-        [FieldOptional]
-        public decimal? weight;
-
-
     }
 }
